6be7ce0be86e2ea87666ec3bc6e175c1f8998bc7
Wallan-P <wallanbezerra@gmail.com>
2025-04-24 17:16:08 +0000

    Nivel-Aventureiro Status-Concluido

--- xadrez.c
+++ xadrez.c
@@ -3,6 +3,8 @@
 int main(){
     int i,j=0;
     
+
+
     printf("movimento da torre\n\n");
 
     for (int i = 0; i < 5; i++){
@@ -11,15 +13,17 @@ int main(){
     };
 
 
+
     printf("\n\nmovimento do bispo \n\n");
 
 
     while ( i < 5){
 
         printf("cima direita\n");
-      i++;
+        i++;
     }
     
+
 
     printf("\n\nmovimento da rainha \n\n");
 
@@ -27,9 +31,24 @@ int main(){
     {
         printf("esquerda\n");
         j++;
-        
+
     } while (j<8);
     
+    
+
+    printf("\n\nmovimento do cavalo \n\n");
+
+
+    for (int i=0,j = 0; i < 1; i++){
+
+        while ( j < 2){
+
+            printf("baixo\n");
+            j++;
+        }
+        printf("esquerda\n");
+    };
+
 
 
 return 0;
